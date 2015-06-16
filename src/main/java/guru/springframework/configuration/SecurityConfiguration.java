f07a7fdc24e6b71afcd119cefe4fd5adfec9181e
John Thompson <fly.yamaha@gmail.com>
2015-06-16 18:39:59 -0400

    adding example files

---
+++ src/main/java/guru/springframework/configuration/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package guru.springframework.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+}
