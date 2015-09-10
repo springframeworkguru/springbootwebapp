7decc819772a3e02c1628c94eaa3be9603d6c6ce
John Thompson <fly.yamaha@gmail.com>
2015-09-10 13:00:35 -0400

    updated for delete method

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -22,7 +22,6 @@ public class ProductController {
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public String list(Model model){
         model.addAttribute("products", productService.listAllProducts());
-        System.out.println("Returning rpoducts:");
         return "products";
     }
 
@@ -46,10 +45,14 @@ public class ProductController {
 
     @RequestMapping(value = "product", method = RequestMethod.POST)
     public String saveProduct(Product product){
-
         productService.saveProduct(product);
-
         return "redirect:/product/" + product.getId();
+    }
+
+    @RequestMapping("product/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        productService.deleteProduct(id);
+        return "redirect:/products";
     }
 
 }
