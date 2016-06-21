b841c128e044706029131b4a221d1dd6352d3b3b
Dennis von der Bey <Dennis@vonderbey.eu>
2016-06-21 08:54:05 +0200

    Fixed a typo
    Thanks for this repo :)

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -22,7 +22,7 @@ public class ProductController {
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public String list(Model model){
         model.addAttribute("products", productService.listAllProducts());
-        System.out.println("Returning rpoducts:");
+        System.out.println("Returning products:");
         return "products";
     }
 
