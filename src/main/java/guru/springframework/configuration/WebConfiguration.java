90f6c9ba0f918ed5841f48038822f668fa49d926
Ximanta <ximaanta.sarma@gmail.com>
2016-12-05 19:41:52 +0530

    Updated ServletRegistrationBean to org.springframework.boot.web.servlet.ServletRegistrationBean

--- src/main/java/guru/springframework/configuration/WebConfiguration.java
+++ src/main/java/guru/springframework/configuration/WebConfiguration.java
@@ -1,7 +1,7 @@
 package guru.springframework.configuration;
 
 import org.h2.server.web.WebServlet;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
