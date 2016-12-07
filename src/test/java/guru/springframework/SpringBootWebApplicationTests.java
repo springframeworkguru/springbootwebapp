4c69c94bf249d681529c51d0a7085dc6e849f7de
Ximanta <ximaanta.sarma@gmail.com>
2016-12-07 19:12:11 +0530

    Updated @SpringApplicationConfiguration to @SpringBootTest. Updated @RunWith(SpringJUnit4ClassRunner.class) declaration with Spring 4.3’s  @RunWith(SpringRunner.class)

--- src/test/java/guru/springframework/SpringBootWebApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootWebApplicationTests.java
@@ -2,12 +2,13 @@ package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootWebApplication.class)
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootWebApplication.class)
 @WebAppConfiguration
 public class SpringBootWebApplicationTests {
 
