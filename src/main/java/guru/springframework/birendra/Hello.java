f3dd37e44a6322ba2fd6b668f3f9d4795f0bf457
Bimal Pathak <bimalpathak20@gmail.com>
2020-02-25 20:42:43 -0600

    Added a hello message

---
+++ src/main/java/guru/springframework/birendra/Hello.java
@@ -0,0 +1,10 @@
+package guru.springframework.birendra;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        int c= a+b;
+        System.out.println("The total sum is : "+ c);
+    }
+}
