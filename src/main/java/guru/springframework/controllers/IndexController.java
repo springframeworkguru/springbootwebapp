49e798eabcef131eb5f52960fd37ca1f8404fdb0
Sanjay-7 <78463536+Sanjay-7@users.noreply.github.com>
2022-10-02 19:11:25 +0530

    Update IndexController.java

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    String index(){
+    public String index(){
         return "index";
     }
 }
